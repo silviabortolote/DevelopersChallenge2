eecfa41322907cbc7426330a667e6f0784ffd550
Silvia Desquiavoni <silviabortolote@gmail.com>
2020-06-14 15:50:49 +0200

    Class addition
    Class addition

---
+++ src/DevelopersChallenge2/ConvertOfx.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace DevelopersChallenge2
+{
+    class ConvertOfx
+    {
+        public List<string> Convert(IEnumerable<XElement> items)
+        {
+            return items.Select(item => item.ToString()).ToList();
+        }
+    }
+
+}
